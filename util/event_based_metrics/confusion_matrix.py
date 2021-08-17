28afb7bde8d10f863fb64f2b3f31d318575f9d39
Robert Voelckner <robert.voelckner@gmail.com>
2021-08-17 15:54:44 +0200

    Confusion matrix now supports "no event"

--- util/event_based_metrics/confusion_matrix.py
+++ util/event_based_metrics/confusion_matrix.py
@@ -18,16 +18,27 @@ __license__ = "MIT"
 
 Scores = NamedTuple("Scores", precision=float, recall=float, f1_score=float)
 
+CLASS_LABELS = [k.name for k in RespiratoryEventType] + ["No event"]
+N_CLASSES = len(RespiratoryEventType) + 1
+
 
 class EventBasedConfusionMatrix:
-    _class_labels = [k.name for k in RespiratoryEventType]
-
     def __init__(self, annotated_events: List[RespiratoryEvent], detected_events: List[RespiratoryEvent]):
         # Generate our confusion matrix
-        data = np.zeros(shape=(len(RespiratoryEventType), len(RespiratoryEventType)), dtype=int)
-        overlaps = get_overlaps(annotated_events=annotated_events, detected_events=detected_events)
-        for o in overlaps:
-            data[o.annotated.event_type.value, o.detected.event_type.value] += 1
+        data = np.zeros(shape=(N_CLASSES, N_CLASSES), dtype=int)
+        detected_events = set(detected_events)
+        for a_ in annotated_events:
+            found_match = False
+            for d_ in detected_events:
+                if a_.overlaps(d_):
+                    detected_events.remove(d_)
+                    data[a_.event_type.value, d_.event_type.value] += 1
+                    found_match = True
+                    break
+            if found_match is False:
+                data[a_.event_type.value, N_CLASSES-1] += 1
+        for d_ in detected_events:
+            data[N_CLASSES-1, d_.event_type.value] += 1
         self._matrix = data  # FIRST DIM: true labels,  SECOND DIM: predicted labels
 
     @classmethod
@@ -47,10 +58,10 @@ class EventBasedConfusionMatrix:
         """
         return self._matrix[index]
 
-    def plot(self, title: Optional[str] = "Confusion matrix for class confidence of detected events", power_norm_gamma=0.3):
+    def plot(self, title: Optional[str] = "Confusion matrix for classification confidence of respiratory events", power_norm_gamma=0.3):
         norm = PowerNorm(gamma=power_norm_gamma, vmin=self._matrix.min(initial=0), vmax=self._matrix.max(initial=1))
-        ax = sns.heatmap(self._matrix, annot=True, fmt="d", norm=norm, xticklabels=self._class_labels,
-                         yticklabels=self._class_labels, cmap=sns.color_palette("Blues"), cbar=False)
+        ax = sns.heatmap(self._matrix, annot=True, fmt="d", norm=norm, xticklabels=CLASS_LABELS,
+                         yticklabels=CLASS_LABELS, cmap=sns.color_palette("Blues"), cbar=False)
         ax.set_yticklabels(labels=ax.get_yticklabels(), va="center")
         # ax.yaxis.tick_right()
         plt.xlabel("Predicted labels", fontweight="bold")
@@ -66,8 +77,8 @@ class EventBasedConfusionMatrix:
         for klass in RespiratoryEventType:
             index = klass.value
             with np.errstate(divide='ignore', invalid='ignore'):  # Helps to suppress divide-by-0-warnings
-                p_ = self._matrix[index, index] / self._matrix[:, index].sum()  # can be NaN!
-                r_ = self._matrix[index, index] / self._matrix[index, :].sum()  # can be NaN!
+                p_ = self._matrix[index, index] / self._matrix[:N_CLASSES-1, index].sum()  # can be NaN!
+                r_ = self._matrix[index, index] / self._matrix[index, :N_CLASSES-1].sum()  # can be NaN!
                 p_ = np.nan_to_num(p_)
                 r_ = np.nan_to_num(r_)
                 f_ = (p_ * r_ * 2) / (p_ + r_)
@@ -88,6 +99,22 @@ class EventBasedConfusionMatrix:
             macro_f1_score = np.nan_to_num(macro_f1_score)
 
         return Scores(precision=float(macro_precision), recall=float(macro_recall), f1_score=float(macro_f1_score))
+
+
+def test_confusion_matrix_generation():
+    annotated_events = [RespiratoryEvent(start=pd.to_timedelta("1 min"), aux_note=None, end=pd.to_timedelta("2 min"), event_type=RespiratoryEventType.Hypopnea),
+                        RespiratoryEvent(start=pd.to_timedelta("3 min"), aux_note=None, end=pd.to_timedelta("4 min"), event_type=RespiratoryEventType.MixedApnea),
+                        RespiratoryEvent(start=pd.to_timedelta("5 min"), aux_note=None, end=pd.to_timedelta("6 min"), event_type=RespiratoryEventType.ObstructiveApnea),
+                        RespiratoryEvent(start=pd.to_timedelta("7 min"), aux_note=None, end=pd.to_timedelta("8 min"), event_type=RespiratoryEventType.Hypopnea)]
+    detected_events = [RespiratoryEvent(start=pd.to_timedelta("1 min"), aux_note=None, end=pd.to_timedelta("2 min"), event_type=RespiratoryEventType.Hypopnea),
+                       RespiratoryEvent(start=pd.to_timedelta("3 min"), aux_note=None, end=pd.to_timedelta("4 min"), event_type=RespiratoryEventType.MixedApnea),
+                       RespiratoryEvent(start=pd.to_timedelta("5 min"), aux_note=None, end=pd.to_timedelta("6 min"), event_type=RespiratoryEventType.ObstructiveApnea),
+                       RespiratoryEvent(start=pd.to_timedelta("7 min"), aux_note=None, end=pd.to_timedelta("8 min"), event_type=RespiratoryEventType.CentralApnea),
+                       RespiratoryEvent(start=pd.to_timedelta("9 min"), aux_note=None, end=pd.to_timedelta("10 min"), event_type=RespiratoryEventType.CentralApnea)]
+    confusion_matrix = EventBasedConfusionMatrix(annotated_events=annotated_events, detected_events=detected_events)
+    assert confusion_matrix._matrix.shape == (N_CLASSES, N_CLASSES)
+    assert confusion_matrix[RespiratoryEventType.Hypopnea.value, RespiratoryEventType.Hypopnea.value] == 1
+    assert confusion_matrix[N_CLASSES-1, RespiratoryEventType.CentralApnea.value] == 1
 
 
 def test_():
