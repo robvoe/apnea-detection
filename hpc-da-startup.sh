34dde8b4bad9b763903e2e9d3fe01de1f35728ca
Robert Voelckner <robert.voelckner@gmail.com>
2021-09-21 11:39:57 +0200

    A few minor bug fixes & added script for environment setup on TUD HPC-DA

---
+++ hpc-da-startup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# srun -p alpha-interactive -N 1 -n 1 -c 40 --gres=gpu:1 --time=08:00:00 --mem-per-cpu=8000 --pty bash   #job submission in ml nodes with allocating: 1 node, 1 task per node, 6 CPUs per task, 1 gpu per node, with 32000 mb on 8 hours.
+
+module load modenv/hiera  GCC/10.2.0  CUDA/11.1.1  OpenMPI/4.0.5 PyTorch/1.9.0
+module load Miniconda3
+
+conda init
+
+# Create new Conda env by cloning from base env
+ENV_NAME="apnea-detection"
+ENV_DIR="$HOME/.conda/envs/$ENV_NAME"
+if [ ! -d $ENV_DIR ]
+then
+  echo "--> Creating Conda env now. That might take a while! <--"
+  conda env create --name $ENV_NAME --file environment.yml
+  conda install -y pytorch cudatoolkit=11.1 -c pytorch -c nvidia
+fi
+
+conda activate $ENV_NAME
--- util/paths.py
+++ util/paths.py
@@ -6,4 +6,4 @@ UTIL_PATH = pathlib.Path(os.path.expanduser(os.path.dirname(os.path.realpath(__f
 PROJECT_BASE_PATH = (UTIL_PATH / "..").resolve()
 
 DATA_PATH = (PROJECT_BASE_PATH / "data").resolve()
-RESULTS_PATH_AI = (PROJECT_BASE_PATH / "ai-based" / "results").resolve()
+RESULTS_PATH_AI = (PROJECT_BASE_PATH / "ai_based" / "results").resolve()
